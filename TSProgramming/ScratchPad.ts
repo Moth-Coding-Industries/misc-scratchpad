cb914fa3837522a17b743f0bc499faa21fffaa09
Eupackardia Calleta <dessmichxx@gmail.com>
2026-08-13 21:33:50 +0000

    Lot's of stuff. npm installed ts-node npx tsc'd --version npm installed #types/node --save-dev npx ts-node LaunchPad.ts

--- TSProgramming/ScratchPad.ts
+++ TSProgramming/ScratchPad.ts
@@ -3,3 +3,11 @@
 */
 
 // V REAL CODE BELOW V
+
+function RunContents(): void { // creates a function named RunContents, everything after { is inside function, void dictates the type of value this function hands back to void
+
+
+console.log("Hello World from TypeScript!")
+
+
+}

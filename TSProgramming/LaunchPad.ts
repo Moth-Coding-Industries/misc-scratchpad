49e3d18f5f1b5c848757d32d1c89ca97c3a15804
Eupackardia Calleta <dessmichxx@gmail.com>
2026-08-13 21:16:48 +0000

    Made LaunchPad and ScratchPad for TypeScript

---
+++ TSProgramming/LaunchPad.ts
@@ -0,0 +1,9 @@
+// Welcome to my launch pad file for TypeScript: LaunchPad.ts! 
+/* Here we write lines of code detailing functions and utilize them with on/off note switches!
+This system works by taking advantage of the fact that notes can make code become ignored during runtime!
+Have fun! (WARNING, VITAL FILE, DO NOT DELETE)
+*/
+
+// V REAL CODE FUNCTIONS BELOW V
+
+// OFF RunContents()
---
+++ TSProgramming/ScratchPad.ts
@@ -0,0 +1,5 @@
+// This is my scratchpad file for TypeScript: ScratchPad.ts!
+/* Here, we write our small experiments in TypeScript before they get their own file! Very fun, I know! 
+*/
+
+// V REAL CODE BELOW V
